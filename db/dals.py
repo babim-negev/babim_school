55af6e5406522bf47fb45e518375fe1d2ea556a6
babim <captainprice1356@gmail.com>
2024-03-05 18:46:05 +0400

    working delete

--- db/dals.py
+++ db/dals.py
@@ -1,6 +1,11 @@
+from typing import Union
+from uuid import UUID
+
+from sqlalchemy import update, and_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db.models import User
+
 
 ###########################################################
 # BLOCK FOR INTERACTION WITH DATABASE IN BUSINESS CONTEXT #
@@ -9,11 +14,12 @@ from db.models import User
 
 class UserDAL:
     """Data Access Layer for operating user info"""
+
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
     async def create_user(
-        self, name: str, surname: str, email: str
+            self, name: str, surname: str, email: str
     ) -> User:
         new_user = User(
             name=name,
@@ -23,3 +29,12 @@ class UserDAL:
         self.db_session.add(new_user)
         await self.db_session.flush()
         return new_user
+
+    async def delete_user(self, user_id: UUID) -> Union[UUID, None]:
+        query = update(User). \
+            where(and_(User.user_id == user_id, User.is_active == True)). \
+            values(is_active=False).returning(User.user_id)
+        res = await self.db_session.execute(query)
+        deleted_user_id_row = res.fetchone()
+        if deleted_user_id_row is not None:
+            return deleted_user_id_row[0]
